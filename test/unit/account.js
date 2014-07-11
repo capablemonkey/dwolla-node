48381b5d1f70878ee6e2110ab80e36011b8fc005
Gordon Zheng <technix1@gmail.com>
2014-07-11 01:28:19 -0400

    add test for basicAccountInfo

--- test/unit/account.js
+++ test/unit/account.js
@@ -7,12 +7,28 @@ describe('Account', function() {
 	describe('get balance', function() {
 		it('Should make the correct request', function(done) {
 			dwolla.setToken(init.fakeKeys.accessToken);
-      dwolla.balance(function() {});
+
+      dwolla.balance(function() {return;});
 
       init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/balance/');
       init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
 
       done();
     });
+	});
+
+	describe('get basicAccountInfo', function() {
+		it('Should make the correct request', function(done) {
+			var FAKE_EMAIL = 'some@email.com';
+			dwolla.basicAccountInfo(FAKE_EMAIL, function() {return;});
+
+			init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/users/' + FAKE_EMAIL);
+			init.restlerMock.lastRequest.options.should.eql({query: {
+				client_id: init.fakeKeys.appKey,
+				client_secret: init.fakeKeys.appSecret
+			}});
+
+			done();
+		});
 	});
 });
