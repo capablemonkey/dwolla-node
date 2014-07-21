c6a050ccb0c15b17f43c2aaddd1b91ce3eb854b2
mach-kernel <ds4028@nyu.edu>
2014-07-21 15:22:20 -0400

    transactions list and by id tests

--- test/unit/transaction.js
+++ test/unit/transaction.js
@@ -6,4 +6,30 @@ var dwolla = require('../../lib/dwolla')(init.fakeKeys.appKey, init.fakeKeys.app
 
 describe('Transaction', function() {
 
+	describe('get transaction by id', function() {
+		it('Should make the correct request', function(done) {
+
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.fundingSourceById('12345678', function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/12345678');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
+	describe('get transactions', function() {
+		it('Should make the correct request', function(done) {
+
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.fundingSourceById(function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
 });
