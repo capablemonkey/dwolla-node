ec4d087d34ec25d4362217bfc3028ae8a899dd68
mach-kernel <ds4028@nyu.edu>
2014-07-22 09:54:45 -0400

    finished transaction test

--- test/unit/transaction.js
+++ test/unit/transaction.js
@@ -10,7 +10,7 @@ describe('Transaction', function() {
 		it('Should make the correct request', function(done) {
 
 		  dwolla.setToken(init.fakeKeys.accessToken);
-	      dwolla.fundingSourceById('12345678', function() {});
+	      dwolla.transactionById('12345678', function() {});
 
 	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/12345678');
 	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
@@ -23,10 +23,49 @@ describe('Transaction', function() {
 		it('Should make the correct request', function(done) {
 
 		  dwolla.setToken(init.fakeKeys.accessToken);
-	      dwolla.fundingSourceById(function() {});
+	      dwolla.transactions(function() {});
 
 	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/');
 	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
+	describe('get transaction stats', function() {
+		it('Should make the correct request', function(done) {
+
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.transactions(function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
+	describe('send transaction', function() {
+		it('Should make the correct request', function(done) {
+
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.send('1234', '812-111-1111', '10.00', function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken, pin: '1234', destinationId: '812-111-1111', amount: '10.00'}});
+
+	      done();
+	    });
+	});
+
+	describe('request transaction', function() {
+		it('Should make the correct request', function(done) {
+
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.request('812-111-1111', '5.00', function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken,  sourceId: '812-111-1111', amount: '5.00'}});
 
 	      done();
 	    });
