982764cac504c59f2ad43f9e76a896899bde585a
mach-kernel <ds4028@nyu.edu>
2014-07-23 15:18:02 -0400

    added tests for new endpoints, new s2s url

--- test/unit/account.js
+++ test/unit/account.js
@@ -15,4 +15,52 @@ describe('Account', function() {
 	      done();
 	    });
 	});
+
+	describe('toggle auto withdrawal', function() {
+		it('Should make the correct request', function(done) {
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.toggleAutoWithdraw(true, '1234567', function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/accounts/features/auto_withdrawal/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken, enabled: true, fundingId: '1234567'}});
+
+	      done();
+	    });
+	});
+
+	describe('get auto withdrawal status', function() {
+		it('Should make the correct request', function(done) {
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.getAutoWithdrawalStatus(function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/accounts/features/auto_withdrawal/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
+	describe('basic account info', function() {
+		it('Should make the correct request', function(done) {
+	      dwolla.basicAccountInfo('812-111-1111', function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/users/812-111-1111');
+	      init.restlerMock.lastRequest.options.should.eql({query: {client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret}});
+
+	      done();
+	    });
+	});
+
+	describe('full account info', function() {
+		it('Should make the correct request', function(done) {
+		  dwolla.setToken(init.fakeKeys.accessToken);
+	      dwolla.balance(function() {});
+
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/users/');
+	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
+
+	      done();
+	    });
+	});
+
 });
