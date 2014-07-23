bebfa53339076e020f73eadbddf56760bbdc7063
Gordon Zheng <technix1@gmail.com>
2014-07-23 17:29:24 -0400

    fix OAuth test

--- test/unit/authentication.js
+++ test/unit/authentication.js
@@ -7,10 +7,10 @@ var dwolla = require('../../lib/dwolla')(init.fakeKeys.appKey, init.fakeKeys.app
 describe('Authentication', function() {
 	describe('request token', function() {
 		it('Should make the correct request', function(done) {
-	      dwolla.requestToken('1234', function() {});
+	      dwolla.requestToken('1234', 'https://fakeredirect.com/', function() {});
 
 	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/v2/token');
-	      init.restlerMock.lastRequest.options.should.eql({query: {client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret, grant_type: 'authorization_code', code: '1234'}});
+	      init.restlerMock.lastRequest.options.should.eql({query: {client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret, grant_type: 'authorization_code', code: '1234', redirect_uri: 'https://fakeredirect.com/'}});
 
 	      done();
 	    });
