bab7e9db8e1330f3a0e8a7c743edd137cadb9e23
Gordon Zheng <technix1@gmail.com>
2014-07-24 16:38:02 -0400

    write tests against new auth methods

--- test/unit/authentication.js
+++ test/unit/authentication.js
@@ -35,12 +35,23 @@ describe('Authentication', function() {
 		});
 	});
 
-	describe('request token', function() {
+	describe('Finish Auth', function() {
 		it('Should make the correct request', function(done) {
-      dwolla.requestToken('1234', 'https://fakeredirect.com/', function() {});
+      dwolla.finishAuth('1234', 'https://fakeredirect.com/', function() {});
 
       init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/v2/token');
-      init.restlerMock.lastRequest.options.should.eql({query: {client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret, grant_type: 'authorization_code', code: '1234', redirect_uri: 'https://fakeredirect.com/'}});
+      init.restlerMock.lastRequest.options.should.eql({client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret, grant_type: 'authorization_code', code: '1234', redirect_uri: 'https://fakeredirect.com/'});
+
+      done();
+    });
+	});
+
+	describe('Refresh Auth', function() {
+		it('Should make the correct request', function(done) {
+      dwolla.refreshAuth('fake_refresh_token', function() {});
+
+      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/v2/token');
+      init.restlerMock.lastRequest.options.should.eql({client_id: init.fakeKeys.appKey, client_secret: init.fakeKeys.appSecret, grant_type: 'refresh_token', refresh_token: 'fake_refresh_token'});
 
       done();
     });
