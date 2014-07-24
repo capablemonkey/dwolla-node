241de0b24d1ed6d31f9315438572c21cd4759480
Gordon Zheng <technix1@gmail.com>
2014-07-23 20:12:33 -0400

    fix transactions tests

--- test/unit/transaction.js
+++ test/unit/transaction.js
@@ -38,7 +38,7 @@ describe('Transaction', function() {
 		  dwolla.setToken(init.fakeKeys.accessToken);
 	      dwolla.transactionsStats(function() {});
 
-	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats/');
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats');
 	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken}});
 
 	      done();
@@ -51,21 +51,21 @@ describe('Transaction', function() {
 		  dwolla.setToken(init.fakeKeys.accessToken);
 	      dwolla.send('1234', '812-111-1111', '10.00', function() {});
 
-	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/stats/');
-	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken, pin: '1234', destinationId: '812-111-1111', amount: '10.00'}});
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/transactions/send');
+	      init.restlerMock.lastRequest.options.should.eql({oauth_token: init.fakeKeys.accessToken, pin: '1234', destinationId: '812-111-1111', amount: '10.00'});
 
 	      done();
 	    });
 	});
 
-	describe('request transaction', function() {
+	describe('make money request', function() {
 		it('Should make the correct request', function(done) {
 
 		  dwolla.setToken(init.fakeKeys.accessToken);
 	      dwolla.request('812-111-1111', '5.00', function() {});
 
 	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/requests/');
-	      init.restlerMock.lastRequest.options.should.eql({query: {oauth_token: init.fakeKeys.accessToken,  sourceId: '812-111-1111', amount: '5.00'}});
+	      init.restlerMock.lastRequest.options.should.eql({oauth_token: init.fakeKeys.accessToken, sourceId: '812-111-1111', amount: '5.00'});
 
 	      done();
 	    });
