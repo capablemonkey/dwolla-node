4c16b96e43a70782907c0b3bc9fe092f54ed61c9
Gordon Zheng <technix1@gmail.com>
2014-07-23 20:06:15 -0400

    fix get checkout URL test

--- test/unit/offsiteGateway.js
+++ test/unit/offsiteGateway.js
@@ -17,7 +17,7 @@ describe('Offsite Gateway', function() {
 	describe('start session', function() {
 		it('Should return the correct boolean', function(done) {
 
-	      dwolla.startGatewaySession('Dummy Redirect URI').should.equal(true);
+	      dwolla.startGatewaySession('http://dummyredirect.com/return').should.equal(true);
 	      done();
 	    });
 	});
@@ -26,7 +26,7 @@ describe('Offsite Gateway', function() {
 		it('Should add the correct item', function(done) {
 
 	      dwolla.addGatewayProduct('Test', 1, 'Test Description', 2).should.equal(true);
-	      dwolla.gatewaySession.should.equal({{'Name': 'Test', 'Description': 'Test Description', 'Price': 1, 'Quantity': 2}});
+	      dwolla.gatewaySession.should.equal([{'Name': 'Test', 'Description': 'Test Description', 'Price': 1, 'Quantity': 2}]);
 
 	      done();
 	    });
@@ -35,26 +35,30 @@ describe('Offsite Gateway', function() {
 	describe('create checkout session', function() {
 		it('Should make the correct request', function(done) {
 
+			// clear any old gateway sessions
+			dwolla.startGatewaySession('http://dummyredirect.com/return');
+
 		  dwolla.addGatewayProduct('Test', 1, 'Test Description', 2).should.equal(true);
 	      dwolla.getGatewayURL('812-111-1111', function() {});
 
-	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/payment/request/');
-	      init.restlerMock.lastRequest.options.should.eql({query: 
+	      init.restlerMock.lastRequest.url.should.equal('https://www.dwolla.com/oauth/rest/offsitegateway/checkouts/');
+	      init.restlerMock.lastRequest.options.should.eql( 
 	      	{
 	      		'Key': init.fakeKeys.appKey,
 	      		'Secret': init.fakeKeys.appSecret, 
-	      		'Test' : false,
-	      		'AllowFundingSources': false,
+	      		'Redirect': 'http://dummyredirect.com/return',
+	      		'Test' : 'false',
+	      		'AllowFundingSources': 'false',
 	      		'PurchaseOrder': {
 	      			'DestinationId': '812-111-1111',
-	      			'OrderItems': {{'Name': 'Test', 'Description': 'Test Description', 'Price': 1, 'Quantity': 2}},
-	      			'Discount': 0,
+	      			'OrderItems': [{'Name': 'Test', 'Description': 'Test Description', 'Price': 1, 'Quantity': 2}],
+	      			'Discount': -0,
 	      			'Shipping': 0,
 	      			'Tax': 0,
 	      			'Total': 2,
 	      			'Notes': ''
 	      		}
-	        }});
+	        });
 
 	      done();
 	    });
